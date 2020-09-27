6096770b0135f624966fdd9039f36eae629212a1
Guilherme Deluca <gdelucapereira@gmail.com>
2020-09-27 20:41:30 -0300

    Adjusts access files from url and exceptions

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 import User from '../models/User';
 
@@ -22,14 +23,15 @@ class AuthenticateUserService {
     const user = await usersRepository.findOne({ where: { email } });
 
     const errorMsg = 'Incorrect email/password combination.';
+    const statusError = 401;
     if (!user) {
-      throw new Error(errorMsg);
+      throw new AppError(errorMsg, statusError);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error(errorMsg);
+      throw new AppError(errorMsg, statusError);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
